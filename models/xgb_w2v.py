001e693d678fb5f93c426d771dae6a26697b5da7
adyanshkakran <adyanshkakran@gmail.com>
2023-11-14 15:19:28 +0530

    minor changes

--- models/xgb_w2v.py
+++ models/xgb_w2v.py
@@ -52,14 +52,14 @@ class XGBModel():
         file_path = f'metrics/{file_name}'
 
         with open(file_path, 'w') as f:
-            f.write(f'Train time: {train_time}\n')
-            f.write(f'Predict time: {predict_time}\n')
-            f.write(f'Accuracy: {accuracy_score(y_test, predictions)}\n')
-            f.write(f'Hamming Score: {1 - hamming_loss(y_test, predictions)}\n')
-            f.write(f'Jaccard Score: {jaccard_score(y_test, predictions, average="micro")}\n')
-            f.write(f'Hit Rate: {hit_rate(y_test, predictions)}\n')
+            f.write(f'Train time: {self.train_time}\n')
+            f.write(f'Predict time: {self.predict_time}\n')
+            f.write(f'Accuracy: {accuracy_score(y_test, self.preds)}\n')
+            f.write(f'Hamming Score: {1 - hamming_loss(y_test, self.preds)}\n')
+            f.write(f'Jaccard Score: {jaccard_score(y_test, self.preds, average="micro")}\n')
+            f.write(f'Hit Rate: {hit_rate(y_test, self.preds)}\n')
             f.write('Classification Report:\n')
-            f.write(f'{classification_report(y_test, predictions, zero_division=True)}\n')
+            f.write(f'{classification_report(y_test, self.preds, zero_division=True)}\n')
 
     def save_model(self):
         self.clf.save_model(f'pretrained/xgb_clf_w2v.json')
@@ -72,58 +72,64 @@ X_test = np.load('embeddings/test_embed.npy', allow_pickle=True)
 _, _, y_train, y_test = load_data()
 
 # multi label prediction
-xgb_classifier = xgb.XGBClassifier(verbosity=2, tree_method="hist", n_jobs=39)
+# xgb_classifier = xgb.XGBClassifier(verbosity=2, tree_method="hist", n_jobs=39)
+xgb_model = XGBModel(error_type="constant")
 
 # model = MultiOutputClassifier(xgb_classifier)
 
-print("Fitting model...")
-st = time.time()
-xgb_classifier.fit(X_train, y_train)
-train_time =  time.time() - st
+print("Training model...")
+xgb_model.fit(X_train, y_train)
+xgb_model.predict(X_test)
+xgb_model.write_metrics(y_test)
 
-st = time.time()
-print("Predicting...")
+# print("Fitting model...")
+# st = time.time()
+# xgb_classifier.fit(X_train, y_train)
+# train_time =  time.time() - st
 
-y_train_prob = xgb_classifier.predict_proba(X_train)
-y_test_prob = xgb_classifier.predict_proba(X_test)
+# st = time.time()
+# print("Predicting...")
 
-predict_time = time.time() - st
-print("Saving metrics...")
+# y_train_prob = xgb_classifier.predict_proba(X_train)
+# y_test_prob = xgb_classifier.predict_proba(X_test)
+
+# predict_time = time.time() - st
+# print("Saving metrics...")
     
-thresholds = []
-for prob, true_labels in zip(y_train_prob, y_train):
-    # errors = np.sum(((prob > prob[:, None]) & (true_labels == 0) | (prob <= prob[:, None]) & (true_labels == 1)) * abs(prob[:, None] - prob), axis=1)
-    errors = np.sum(((prob > prob[:, None]) & (true_labels == 0) | (prob <= prob[:, None]) & (true_labels == 1)), axis=1)
-    thresholds.append(prob[np.argmin(errors)])
+# thresholds = []
+# for prob, true_labels in zip(y_train_prob, y_train):
+#     # errors = np.sum(((prob > prob[:, None]) & (true_labels == 0) | (prob <= prob[:, None]) & (true_labels == 1)) * abs(prob[:, None] - prob), axis=1)
+#     errors = np.sum(((prob > prob[:, None]) & (true_labels == 0) | (prob <= prob[:, None]) & (true_labels == 1)), axis=1)
+#     thresholds.append(prob[np.argmin(errors)])
 
-xgb_regressor = xgb.XGBRegressor(verbosity=2, tree_method="hist", n_jobs=39)
+# xgb_regressor = xgb.XGBRegressor(verbosity=2, tree_method="hist", n_jobs=39)
 
-xgb_regressor.fit(y_train_prob, thresholds)
+# xgb_regressor.fit(y_train_prob, thresholds)
 
-y_test_thresh = xgb_regressor.predict(y_test_prob)
+# y_test_thresh = xgb_regressor.predict(y_test_prob)
 
-predictions = (y_test_prob >= y_test_thresh[:, None]).astype(int)
+# predictions = (y_test_prob >= y_test_thresh[:, None]).astype(int)
 
-print(f'Hit Rate: {hit_rate(y_test, predictions)}')
-print(f'Jaccard Score: {jaccard_score(y_test, predictions, average="micro")}')
+# print(f'Hit Rate: {hit_rate(y_test, predictions)}')
+# print(f'Jaccard Score: {jaccard_score(y_test, predictions, average="micro")}')
 
 
-file_name = f'xgb_{datetime.now().strftime("%Y%m%d%H%M")}.txt'
+# file_name = f'xgb_{datetime.now().strftime("%Y%m%d%H%M")}.txt'
 
-file_path = f'metrics/{file_name}'
+# file_path = f'metrics/{file_name}'
 
-with open(file_path, 'w') as f:
-    f.write(f'Train time: {train_time}\n')
-    f.write(f'Predict time: {predict_time}\n')
-    f.write(f'Accuracy: {accuracy_score(y_test, predictions)}\n')
-    f.write(f'Hamming Score: {1 - hamming_loss(y_test, predictions)}\n')
-    f.write(f'Jaccard Score: {jaccard_score(y_test, predictions, average="micro")}\n')
-    f.write(f'Hit Rate: {hit_rate(y_test, predictions)}\n')
-    f.write('Classification Report:\n')
-    f.write(f'{classification_report(y_test, predictions, zero_division=True)}\n')
-    # f.write(f'Confusion Matrix: {multilabel_confusion_matrix(y_test, y_pred)}\n')
+# with open(file_path, 'w') as f:
+#     f.write(f'Train time: {train_time}\n')
+#     f.write(f'Predict time: {predict_time}\n')
+#     f.write(f'Accuracy: {accuracy_score(y_test, predictions)}\n')
+#     f.write(f'Hamming Score: {1 - hamming_loss(y_test, predictions)}\n')
+#     f.write(f'Jaccard Score: {jaccard_score(y_test, predictions, average="micro")}\n')
+#     f.write(f'Hit Rate: {hit_rate(y_test, predictions)}\n')
+#     f.write('Classification Report:\n')
+#     f.write(f'{classification_report(y_test, predictions, zero_division=True)}\n')
+#     # f.write(f'Confusion Matrix: {multilabel_confusion_matrix(y_test, y_pred)}\n')
 
-# # save model
-print("saving model..")
-xgb_classifier.save_model(f'pretrained/xgb_w2v.json')
-print("model saved..")
+# # # save model
+# print("saving model..")
+# xgb_classifier.save_model(f'pretrained/xgb_w2v.json')
+# print("model saved..")
