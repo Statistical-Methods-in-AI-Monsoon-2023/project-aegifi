ecc8b40bf3d82783afe4f97017f0efb448f15a7c
Akshit Sinha <akshitsinha3@gmail.com>
2023-11-14 10:21:44 +0530

    create xgb class

--- models/utils.py
+++ models/utils.py
@@ -1,11 +1,8 @@
 # utils file
 
-import pandas as pd
 import numpy as np
 import scipy.sparse
 from sklearn.model_selection import train_test_split
-import cupy as cp
-import cupyx.scipy.sparse as csp
 
 def load_data():
     X = scipy.sparse.load_npz('vectorised_data/X.npz')
@@ -14,11 +11,6 @@ def load_data():
 
     # split data into train and test
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    #print(X.shape, y.shape)
-    #X_train = X[:200000]
-    #X_test = X[200000:]
-    #y_train = y[:200000]
-    #y_test = y[200000:]
     print('Loaded data')
 
     return X_train, X_test, y_train, y_test
--- models/xgb_w2v.py
+++ models/xgb_w2v.py
@@ -1,15 +1,69 @@
 import xgboost as xgb
-from sklearn.multioutput import MultiOutputClassifier
 from utils import load_data, hit_rate
 import time
 from datetime import datetime
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, hamming_loss, multilabel_confusion_matrix, jaccard_score, classification_report
-import pickle
+from sklearn.metrics import accuracy_score, hamming_loss, jaccard_score, classification_report
 import numpy as np
 
-def softmax(x):
-    e_x = np.exp(x - np.max(x))
-    return e_x / e_x.sum(axis=0)
+class XGBModel():
+    def __init__(self, error_type="constant"):
+        self.clf = xgb.XGBClassifier(verbosity=2, tree_method="hist", n_jobs=39)
+        self.reg = xgb.XGBRegressor(verbosity=2, tree_method="hist", n_jobs=39)
+        self.train_time = 0
+        self.predict_time = 0
+        self.error_type = error_type
+        self.preds = None
+    
+    def error(self, a, b):
+        if self.error_type == "constant":
+            return 1
+        if self.error_type == "absolute":
+            return abs(a-b)
+        if self.error_type == "squared":
+            return (a-b)**2
+    
+    def get_thresholds(self, y_prob, y):
+        thresholds = []
+        for prob, true_labels in zip(y_prob, y):
+            errors = np.sum(((prob > prob[:, None]) & (true_labels == 0) | (prob <= prob[:, None]) & (true_labels == 1)) * self.error(prob[:, None],prob), axis=1)
+            thresholds.append(prob[np.argmin(errors)])
+        return thresholds
+
+    def fit(self, X, y):
+        st = time.time()
+        self.clf.fit(X, y)
+        y_prob = self.clf.predict_proba(X)
+        thresholds = self.get_thresholds(y_prob, y)
+        self.reg.fit(y_prob, thresholds)
+        self.train_time = time.time() - st
+    
+    def predict(self, X):
+        st = time.time()
+        y_prob = self.clf.predict_proba(X)
+        y_thresh = self.reg.predict(y_prob)
+        y_pred = (y_prob >= y_thresh[:, None]).astype(int)
+        self.predict_time = time.time() - st
+        self.preds = y_pred
+        return y_pred
+    
+    def write_metrics(self, y_test):
+        file_name = f'xgb_{datetime.now().strftime("%Y%m%d%H%M")}.txt'
+
+        file_path = f'metrics/{file_name}'
+
+        with open(file_path, 'w') as f:
+            f.write(f'Train time: {train_time}\n')
+            f.write(f'Predict time: {predict_time}\n')
+            f.write(f'Accuracy: {accuracy_score(y_test, predictions)}\n')
+            f.write(f'Hamming Score: {1 - hamming_loss(y_test, predictions)}\n')
+            f.write(f'Jaccard Score: {jaccard_score(y_test, predictions, average="micro")}\n')
+            f.write(f'Hit Rate: {hit_rate(y_test, predictions)}\n')
+            f.write('Classification Report:\n')
+            f.write(f'{classification_report(y_test, predictions, zero_division=True)}\n')
+
+    def save_model(self):
+        self.clf.save_model(f'pretrained/xgb_clf_w2v.json')
+        self.reg.save_model(f'pretrained/xgb_reg_w2v.json')
 
 # load from embeddings
 print("Loading data...")
