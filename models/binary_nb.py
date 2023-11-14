4dec534bb90b37b3c201fb0a47fed0458cfac0bc
Akshit Sinha <akshitsinha3@gmail.com>
2023-11-14 17:43:50 +0530

    big brain

--- models/binary_nb.py
+++ models/binary_nb.py
@@ -37,3 +37,48 @@ with open(file_path, 'w') as f:
     f.write(f'Hit Rate: {hit_rate(y_test, y_pred)}\n')
     f.write('Classification Report:\n')
     f.write(f'{classification_report(y_test, y_pred, zero_division=True)}\n')
+
+class BinaryNBRunner:
+    def __init__(self, load_models=False):
+        self.X_train = None
+        self.X_test = None
+        self.y_train = None
+        self.y_test = None
+        self.model = OneVsRestClassifier(BernoulliNB())
+        self.train_time = 0
+        self.predict_time = 0
+        self.preds = None
+        
+    def load_data(self):
+        print("loading data...")
+        self.X_train, self.X_test, self.y_train, self.y_test = load_data()
+        print("loaded data")
+    
+    def run_training(self):
+        self.load_data()
+        print("Training...")
+        self.train_time = time()
+        self.model.fit(self.X_train, self.y_train)
+        self.train_time = time() - self.train_time
+        print("Trained")
+    
+    def write_metrics(self):
+        file_name = f'binary_nb_{datetime.now().strftime("%Y%m%d%H%M")}.txt'
+        file_path = f'./metrics/{file_name}'
+        with open(file_path, 'w') as f:
+            f.write(f'Train time: {train_time}\n')
+            f.write(f'Predict time: {predict_time}\n')
+            f.write(f'Accuracy: {accuracy_score(self.y_test, self.preds)}\n')
+            f.write(f'Hamming Score: {1 - hamming_loss(self.y_test, self.preds)}\n')
+            f.write(f'Jaccard Score: {jaccard_score(self.y_test, self.preds, average="micro")}\n')
+            f.write(f'Hit Rate: {hit_rate(self.y_test, self.preds)}\n')
+            f.write('Classification Report:\n')
+            f.write(f'{classification_report(self.y_test, self.preds, zero_division=True)}\n')
+    
+    def run_inference(self):
+        print("Predicting...")
+        self.predict_time = time()
+        self.preds = self.model.predict(self.X_test)
+        self.predict_time = time() - self.predict_time
+        self.write_metrics()
+        print("Predicted")
--- models/main.py
+++ models/main.py
@@ -1,6 +1,18 @@
 from xgb_w2v import XGBRunner
+from binary_nb import BinaryNBRunner
+from multinomial_nb import MultinomailNBRunner
+class Model:
+    def __init__(self, model='xgb', word_embeddings='w2v', load_models=False):
+        if model == 'xgb':
+            self.model = XGBRunner(word_embeddings=word_embeddings, load_models=load_models)
+        elif model == 'bnb':
+            self.model = BinaryNBRunner(load_models=load_models)
+        elif model == 'mnb':
+            self.model = MultinomailNBRunner(load_models=load_models)
+        else:
+            raise Exception('Invalid model name')
 
 if __name__ == '__main__':
-    runner = XGBRunner(word_embeddings='TF-IDF')
-    runner.run_training(save_model=True)
+    md = Model(model='xgb', word_embeddings='w2v', load_models=False)
+    md.model.run_training(save_model=True)
     # runner.run_inference()
