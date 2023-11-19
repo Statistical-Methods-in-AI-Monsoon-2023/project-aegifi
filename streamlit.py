0450fd792f946b2bc9cae528d4d6bed42c22ec40
Akshit Sinha <akshitsinha3@gmail.com>
2023-11-20 01:30:32 +0530

    make streamlit more fun

--- streamlit.py
+++ streamlit.py
@@ -6,27 +6,32 @@ sys.path.append('./src/')
 sys.path.append('./utils/')
 
 from main import streamlit_run
+
+st.set_page_config(layout="wide")
+
 st.title("Team 13 - AegiFi")
+
+# header
+st.markdown('## Predicting Movie Genres from Plot Summaries')
 
 st.markdown('---')
 
-models = st.multiselect(
-    'Select the models you want to run inference on:',
-    ['Binary Naive Bayes', 'Multinomial Naive Bayes', 'XGBoost', 'Binary GRU', 'Rank GRU', 'Multinomial GRU'],
-)
+col1,_, col2 = st.columns((2,1,2))
 
-# show the selected models as bullet points
-st.markdown('#### Selected models:')
-st.markdown('\n'.join(['- ' + model for model in models]))
-
-word_embeddings = 'Word 2 Vec'
-if 'XGBoost' in models:
-    # add a button to select the word embeddings with word2vec selected by default
-    word_embeddings = st.radio(
-        'Select the word embeddings to use:',
-        ('Word 2 Vec', 'TF-IDF', 'Bag of Words'),
-        index=0
+with col1:
+    models = st.multiselect(
+        'Select the models you want to run inference on:',
+        ['Binary Naive Bayes', 'Multinomial Naive Bayes', 'XGBoost', 'Binary GRU', 'Rank GRU', 'Multinomial GRU'],
     )
+
+    word_embeddings = 'Word 2 Vec'
+    if 'XGBoost' in models:
+        # add a button to select the word embeddings with word2vec selected by default
+        word_embeddings = st.radio(
+            'Select the word embeddings to use with XGBoost:',
+            ('Word 2 Vec', 'TF-IDF', 'Bag of Words'),
+            index=0
+        )
 
 model_code = {
     "Binary Naive Bayes": "bnb",
@@ -43,10 +48,13 @@ embed_code = {
     'Bag of Words': 'bow'
 }
 
-run_training = st.button('Run Training', disabled = len(models) == 0)
 
-# add button to run inference 
-run_inference = st.button('Run Inference', disabled = len(models) == 0)
+with col2:
+    inner_col1, inner_col2 = st.columns((1,1))
+    with inner_col1:
+        run_training = st.button('Run Training', disabled = len(models) == 0)
+    with inner_col2:
+        run_inference = st.button('Run Inference', disabled = len(models) == 0)
 
 # enable the button only if at least one model is selected
 
@@ -58,29 +66,30 @@ if run_inference:
             if file.endswith('.txt'):
                 metrics_files.append(os.path.join(root, file))
     
-    print(metrics_files)
     
-    st.write('Runnning inference...')
-    streamlit_run([model_code[model] for model in models], word_embeddings=embed_code[word_embeddings], load_models=True)
-    st.write('Done!')
+    with col2:
+        with st.spinner('Inference in progress...'):
+            streamlit_run([model_code[model] for model in models], word_embeddings=embed_code[word_embeddings], load_models=True)
+        st.balloons()
     
-    # check if new metrics files have been added
-    new_metrics_files = []
-    for root, dirs, files in os.walk('./src/'):
-        for file in files:
-            if file.endswith('.txt'):
-                if os.path.join(root, file) not in metrics_files:
-                    new_metrics_files.append(os.path.join(root, file))
+        # check if new metrics files have been added
+        new_metrics_files = []
+        for root, dirs, files in os.walk('./src/'):
+            for file in files:
+                if file.endswith('.txt'):
+                    if os.path.join(root, file) not in metrics_files:
+                        new_metrics_files.append(os.path.join(root, file))
 
-    # open the new metrics files and display the contents
-    if len(new_metrics_files) > 0:
-        st.markdown('#### New metrics files:')
-        for file in new_metrics_files:
-            with open(file, 'r') as f:
-                st.markdown(f'##### {file}')
-                st.markdown(f'```{f.read()}```')
+        # open the new metrics files and display the contents
+        if len(new_metrics_files) > 0:
+            st.markdown('#### New metrics files:')
+            for file in new_metrics_files:
+                with open(file, 'r') as f:
+                    st.markdown(f'##### {file}')
+                    st.markdown(f'```{f.read()}```')
                 
 if run_training:
-    st.write('Runnning training...')
-    streamlit_run([model_code[model] for model in models], word_embeddings=embed_code[word_embeddings], load_models=False)
-    st.write('Done!')
+    with col2:
+        with st.spinner('Training in progress...'):
+            streamlit_run([model_code[model] for model in models], word_embeddings=embed_code[word_embeddings], load_models=False)
+        st.ballons()
